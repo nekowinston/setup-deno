e8a55133053b152927e8b4f51414905c5635c7b3
winston <hey@winston.sh>
2023-12-26 18:16:08 +0100

    fix: silly

--- bundle.ts
+++ bundle.ts
@@ -2,12 +2,20 @@
 import { bundle } from "https://deno.land/x/emit@0.32.0/mod.ts";
 import { ensureDir } from "https://deno.land/std@0.209.0/fs/ensure_dir.ts";
 
+const shebang = await Deno.readTextFileSync("./cache.ts").split("\n")[0];
 const bundled = await bundle(
   new URL("./cache.ts", import.meta.url),
   { minify: true },
 );
 
 ensureDir("./dist");
-await Deno.writeTextFile("./dist/cache.js", bundled.code, {
-  mode: 0o744,
-});
+await Deno.writeTextFile(
+  "./dist/cache.js",
+  [
+    shebang.startsWith("#!") ? shebang : "#!/usr/bin/env -S deno run -A",
+    bundled.code,
+  ].join("\n"),
+  {
+    mode: 0o744,
+  },
+);
